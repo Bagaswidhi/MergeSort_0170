aba778a744ed5aeb801f246858a4147c059653f9
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-04-17 10:24:56 +0700

    Mendekalarasikan prosedur input data merge sort

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -4,6 +4,32 @@ using namespace std;
 int arr[20], B[20]; // array utama dan array sementara
 int n; // ukuran array
 
+void input() // Prosedur input data
+{
+    while(true)
+    {
+        cout << "Masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else 
+        {
+            cout << "\n Maksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n========================" << endl;
+    cout << "Inputkan isi element array" << endl;
+    cout << "==========================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
 int main()
 {
 
